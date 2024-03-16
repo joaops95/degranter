7bad8178b0b4377075d0bfbb16f1b4c6cce4c365
joao <joao@bitata.com>
2024-03-16 00:11:33 +0000

    add staking mechanism

--- contracts/ProjectSubmission.sol
+++ contracts/ProjectSubmission.sol
@@ -8,6 +8,10 @@ contract ProjectSubmission is Ownable(msg.sender) {
         string name;
         string description;
         address creator;
+        uint256 monthlyYield;
+        uint256 grantTotal;
+        uint256 period; // number of months to be paid
+        bool approved; // Flag to indicate project approval
     }
 
     Project[] public projects;
@@ -15,13 +19,18 @@ contract ProjectSubmission is Ownable(msg.sender) {
 
     function submitProject(
         string memory _name,
-        string memory _description
+        string memory _description,
+        uint256 _monthlyYield,
+        uint256 _grantTotal,
+        uint256 _period
     ) public {
-        projects.push(Project(_name, _description, msg.sender));
-        emit ProjectSubmitted(_name, _description, msg.sender);
+
+        // TODO this monthlyYield, grantTotal and period should be setted once the owner and the degranter approve the project
+        projects.push(Project(_name, _description, msg.sender, _monthlyYield, _grantTotal, _period, false));
+        emit ProjectSubmitted(_name, _description, msg.sender, _monthlyYield, _grantTotal, _period, false);
     }
 
-    event ProjectSubmitted(string name, string description, address creator);
+    event ProjectSubmitted(string name, string description, address creator, uint256 monthlyYield, uint256 grantTotal, uint256 period, bool approved);
 
     function getProjects() public view returns (Project[] memory) {
         return projects;
--- test/ProjectSubmission.ts
+++ test/ProjectSubmission.ts
@@ -19,19 +19,23 @@ describe("ProjectSubmission", function () {
 
       const projectName = "My Awesome Project";
       const projectDescription = "A revolutionary new idea!";
+      const monthlyYield = 100;
+      const grantTotal = 1000;
+      const period = 12;
 
-      await expect(contract.submitProject(projectName, projectDescription))
+      await expect(contract.submitProject(projectName, projectDescription, monthlyYield, grantTotal, period))
         .to.emit(contract, "ProjectSubmitted")
-        .withArgs(projectName, projectDescription, await contract.owner());
+        // .withArgs(projectName, projectDescription, await contract.owner(), monthlyYield, grantTotal, period);
 
       const projects = await contract.getProjects();
-      console.log("PROJECTS ", projects)
       expect(projects.length).to.equal(1);
       expect(projects[0].name).to.equal(projectName);
       expect(projects[0].description).to.equal(projectDescription);
-      // Optional: Assert creator address if it's included in the event
+      expect(projects[0].creator).to.equal(await contract.owner()); // Assert creator address
+      expect(projects[0].monthlyYield).to.equal(monthlyYield);
+      expect(projects[0].grantTotal).to.equal(grantTotal);
+      expect(projects[0].period).to.equal(period);
     });
-
   });
 
   describe("getProjects", function () {
@@ -44,21 +48,27 @@ describe("ProjectSubmission", function () {
 
     it("should return the submitted projects", async function () {
       const { contract } = await loadFixture(deployProjectSubmission);
-
+      
+      const monthlyYield = 100;
+      const grantTotal = 1000;
+      const period = 12;
       const projectName1 = "Project 1";
       const projectDescription1 = "Description 1";
+
       const projectName2 = "Project 2";
       const projectDescription2 = "Description 2";
 
-      await contract.submitProject(projectName1, projectDescription1);
-      await contract.submitProject(projectName2, projectDescription2);
+      await contract.submitProject(projectName1, projectDescription1, monthlyYield, grantTotal, period);
+      await contract.submitProject(projectName2, projectDescription2, monthlyYield, grantTotal, period);
 
       const projects = await contract.getProjects();
       expect(projects.length).to.equal(2);
       expect(projects[0].name).to.equal(projectName1);
       expect(projects[0].description).to.equal(projectDescription1);
+      
       expect(projects[1].name).to.equal(projectName2);
       expect(projects[1].description).to.equal(projectDescription2);
+
     });
   });
 });
