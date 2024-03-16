1edd72a0e2b5b12f8eafb319749002f8e628a04e
afonsocrg <afonso.crg@gmail.com>
2024-03-16 10:02:10 +0000

    redirect to home page when wallet disconnected

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,9 +1,19 @@
 import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
+import { useAccount } from "wagmi";
 
 import Home from "@pages/Home";
 import Startup from "@pages/Startup";
 import Investor from "@pages/Investor";
+
+function PrivateRoute() {
+  const { isConnected } = useAccount();
+  if (isConnected) {
+    return <Outlet />;
+  } else {
+    return <Navigate to={"/"} />;
+  }
+}
 
 export function AppRoutes() {
   // prettier-ignore
@@ -12,8 +22,10 @@ export function AppRoutes() {
       <Routes>
         <Route path='/'>
           <Route index                        element={<Home/>                        } />
-          <Route path="startup"               element={<Startup/>                     } />
-          <Route path="investor"              element={<Investor/>                    } />
+          <Route element={<PrivateRoute/>}>
+            <Route path="startup"               element={<Startup/>                     } />
+            <Route path="investor"              element={<Investor/>                    } />
+          </Route>
         </Route>
       </Routes>
     </Suspense>
